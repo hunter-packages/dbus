b55da083f15cfb73f984aaca5bef0f1575dcf266
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2015-09-15 09:25:38 +0200

    Fixes CMake to allow having a top-level CMakeLists.txt for dbus, easing integration in ExternalProject_Add used by ruslo/hunter package manager.

--- cmake/ConfigureChecks.cmake
+++ cmake/ConfigureChecks.cmake
@@ -174,7 +174,7 @@ if (DBUS_ENABLE_ABSTRACT_SOCKETS)
 
   try_compile(HAVE_ABSTRACT_SOCKETS
               ${CMAKE_BINARY_DIR}
-              ${CMAKE_SOURCE_DIR}/modules/CheckForAbstractSockets.c)
+              ${dbus_SOURCE_DIR}/modules/CheckForAbstractSockets.c)
 
 endif(DBUS_ENABLE_ABSTRACT_SOCKETS)
 
